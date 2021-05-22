10211a3f20613ed14a17e04c8a13603dab537941
prabin-acharya@ <acharyaprabin101@gmail.com>
2021-05-22 15:18:05 +0545

    added titlepage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,5 +6,16 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+
     })
+
+
+def EntryPage(request, title):
+    #if request.method == "POST":
+    #    title = request.POST    
+
+    return render(request, "encyclopedia/entrypage.html", {
+        "title" : title,
+        "getentry": util.get_entry(title)
+        })
 
