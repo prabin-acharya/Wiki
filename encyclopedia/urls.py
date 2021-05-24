ba8cfd4267288c207bad8a4e4c7c4edd9f4bbb4b
prabin-acharya@ <acharyaprabin101@gmail.com>
2021-05-24 20:37:42 +0545

    Search Implementation

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>/", views.EntryPage, name='entrypage')
+    path("wiki/<str:title>/", views.titlepage_view, name="titlepageview"),
+    re_path(r"^wiki/(?P<title>).*[\s\w]*/$", views.titlepage_view, name="titlepageview"),
+    path("addpage", views.addpage_view, name='addpage'),
+    path("wiki/entrynotfound/", views.notFound, name='entrynotfound')
     ]
+
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,21 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
 
 from . import util
 
+#    search = forms.CharField(label="", 
+#        widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
+
+#class AddPageForm(forms.Form):
+#    title = forms.CharField(label="Title : ", 
+#        widget=forms.TextInput(attrs={'placeholder': 'Enter Title of the page'}))
+#    newpage = forms.CharField(label="", 
+#        widget=forms.Textarea(attrs={'placeholder': 'Type article here.'}))
+
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    entry_list = util.list_entries()
+    if request.method == "POST":
+        search = request.POST.get("search")
 
-    })
+        if search is not None:
+            search = search.lower().strip()
+            for entry in entry_list:
+                if search == entry.lower():
+                    return redirect('titlepageview', title=entry)
+    
+
+    context = {"entries": util.list_entries()}
+    return render(request, "encyclopedia/index.html", context)
 
 
-def EntryPage(request, title):
-    #if request.method == "POST":
-    #    title = request.POST    
+def titlepage_view(request, title):
+    entry_list = util.list_entries()
+    if title is not None :
+        title = title.lower().strip()
+        for entry in entry_list:
+            if title == entry.lower():
+                context = {"title" : entry, "getentry": util.get_entry(entry)}
+                return render(request, "encyclopedia/titlepage.html", context)
 
-    return render(request, "encyclopedia/entrypage.html", {
-        "title" : title,
-        "getentry": util.get_entry(title)
-        })
+
+    context = {"title" : title}
+    return render(request, "encyclopedia/entrynotfound.html", context)
+
+def notFound(request):
+    return render(request, "encyclopedia/entrynotfound.html")
+
+
+def addpage_view(request):
+    context = {"searchform": SearchForm()}
+    return render(request, "encyclopedia/addpage.html", context)
+
+
 
